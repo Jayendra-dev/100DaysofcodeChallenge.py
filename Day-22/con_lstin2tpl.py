0421344bf745747166900517e10cf3f3698783be
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-06-17 17:17:56 +0530

    con_lstin2tpl.py
    # convert list in tuple using tuple function 
    lst=[1,2,3,4]
    print(lst)
    print(type(lst))
    print("converting into tuple")
    tpl=tuple(lst)
    print("After converting")
    print(tpl)
    print(type(tpl))

---
+++ Day-22/con_lstin2tpl.py
@@ -0,0 +1,9 @@
+# convert list in tuple using tuple function 
+lst=[1,2,3,4]
+print(lst)
+print(type(lst))
+print("converting into tuple")
+tpl=tuple(lst)
+print("After converting")
+print(tpl)
+print(type(tpl))
