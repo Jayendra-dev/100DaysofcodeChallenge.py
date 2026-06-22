4661a25ce6d55734a2bdf3f811bba0a6a0279489
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-06-22 14:27:29 +0530

    Day-24/Day-25 directory

--- Day-24/Day-25/replace.py
+++
@@ -1,7 +0,0 @@
-# program to replace a word in a file
-with open("file.txt","r") as file:
-    data=file.read()
-data=data.replace("python","java")
-with open("file.txt","w") as file:
-    file.write(data)
-print ("word replaced successfully")
