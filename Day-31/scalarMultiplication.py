29337c11ea0c5c47bf02ab79fd840cffa173df65
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-08-09 00:35:48 +0530

    Add comments to scalarMultiplication.py
    Added comments explaining the purpose of the script and its date.

---
+++ Day-31/scalarMultiplication.py
@@ -0,0 +1,3 @@
+# As ,I am aspired to become AI engineer its one step towards my goal 
+#so today dated 09/08/2026.I solved some basic problems of scalar multiplication 
+# of linear algebra in numpy
