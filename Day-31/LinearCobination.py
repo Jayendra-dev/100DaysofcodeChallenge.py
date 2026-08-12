a679a036b156c88fb819fc6cfe7bcdc3b5f6d131
Jayendra Kumar <jayendrakumar.mca@gmail.com>
2026-08-13 02:24:35 +0530

    Add files via upload

---
+++ Day-31/LinearCobination.py
@@ -0,0 +1,8 @@
+import numpy as np
+a = np.array([1, 2, 3])
+b = np.array([4, 0, -1])
+
+result = 3*a - 2*b
+print("Q2:", result)
+# Output: [-5 6 11]
+# Algebra: 3*[1,2,3] - 2*[4,0,-1] = [3,6,9] - [8,0,-2]
